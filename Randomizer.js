9080406b2d0a83093acb5bbc13a5b252a5555636
Paauloccp <paauloccp@gmail.com>
2024-03-03 19:45:44 -0300

    commit 1

---
+++ Randomizer.js
@@ -0,0 +1,28 @@
+const lista = [
+
+]
+function Add() {
+    const input = document.getElementById("input")
+    const texto = input.value
+    lista.push(texto)
+    localStorage.setItem("listaserie", JSON.stringify(lista))
+    // alert(JSON.stringify(lista))
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const listasalva = localStorage.getItem("listaserie")
+    if (listasalva !== null) {
+        const nando = JSON.parse(listasalva)
+        nando.forEach(element => {
+            lista.push(element)
+        });
+        // alert(JSON.stringify(lista))
+    }
+})
+function Random() {
+    const miguel = Math.floor(Math.random() * lista.length)
+    const selectedTVSHOW = lista[miguel]
+    alert(selectedTVSHOW)
+}
+function Apagao() {
+
+}
